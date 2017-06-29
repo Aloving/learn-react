fb64fc5c9f91e86e94844ecc8667ca01cab63fb1
Aloving <Ilegend2011@gmail.com>
2017-06-30 00:53:09 +0400

    jsx

---
+++ app.jsx
@@ -0,0 +1,9 @@
+var jsx = 
+<div className="container">
+	<img src="https://facebook.github.io/react/img/logo.svg" />
+	<h1>React</h1>
+	<p>Библиотека для создания пользовательских интерфейсов</p>
+</div>;
+
+
+ReactDOM.render(jsx, document.getElementById('root'));
