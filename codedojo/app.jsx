c45a803d7f5c46ebab31b09c49331c597347f0ed
Aloving <Ilegend2011@gmail.com>
2017-06-30 09:42:27 +0400

    props

--- codedojo/app.jsx
+++ codedojo/app.jsx
@@ -1,11 +1,25 @@
-function Hero() {
+function Hero(props) {
 	return (
 		<div className="container">
-			<img src="https://facebook.github.io/react/img/logo.svg" />
-			<h1> React </h1>
-			<p>Библиотека для создания пользовательских интерфейсов</p>
+			<img src={props.imageUrl} />
+			<h1>{props.title}</h1>
+			<p>{props.subtitle}</p>
 		</div>
 	);
 }
 
-ReactDOM.render(<Hero />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Hero
+			title="React"
+			subtitle="Библиотека для создания пользовательских интерфейсов"
+			imageUrl="https://facebook.github.io/react/img/logo.svg"
+		/>
+		<Hero
+			title="Angular 2"
+			subtitle="Один фреймворк"
+			imageUrl="https://facebook.github.io/react/img/logo.svg"
+		/>
+	</div>,
+	document.getElementById('root')
+);
