541666619dda91bdda48aa9b11e21d67d378e6ec
Aloving <Ilegend2011@gmail.com>
2017-07-04 09:45:40 +0400

    dojo react-basics #components - jsx features

--- codedojo-react-basics/src/App.jsx
+++ codedojo-react-basics/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import todos from './todos';
+
 // components
 import Header from './components/Header';
 import Todo from './components/Todo';
@@ -10,8 +12,10 @@ function App(props) {
 		<main>
 			<Header title={props.title} />
 			<section className="todo-list">
-				<Todo title={"Изучить javascript"} completed={true}/>
-				<Todo title={"Изучить React"} completed={false}/>
+				{props.todos.map(todo =>
+					<Todo title={todo.title} completed={todo.completed} key={todo.id}/>
+				)}
+
 			</section>
 		</main>
 	);
@@ -19,10 +23,17 @@ function App(props) {
 
 App.propTypes = {
 	title: React.PropTypes.string,
+	todos: React.PropTypes.arrayOf(
+		React.PropTypes.shape({
+			id: React.PropTypes.number.isRequired,
+			title: React.PropTypes.string.isRequired,
+			completed: React.PropTypes.bool.isRequired,
+		})
+	).isRequired,
 };
 
 App.defaultProps = {
 	title: 'React TODO',
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App todos={todos} />, document.getElementById('root'));
