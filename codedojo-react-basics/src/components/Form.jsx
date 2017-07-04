e01dc839afcee92937012d7e4d83a5dcc6b0a0e8
Aloving <Ilegend2011@gmail.com>
2017-07-04 12:07:31 +0400

    dojo react-basics #forms - controlled components

--- codedojo-react-basics/src/components/Form.jsx
+++ codedojo-react-basics/src/components/Form.jsx
@@ -6,25 +6,38 @@ class Form extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			title: '',
+		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleSubmit(event) {
 		event.preventDefault();
-		let title = this.refs.title.value;
+		let title = this.state.title;
 
-		if(title){
+		if (title) {
 			this.props.onAdd(title);
 			this.refs.title.value = '';
 		}
 	}
 
+	handleChange(event) {
+		let title = event.target.value;
+		this.setState({ title });
+	}
+
 	render() {
 		return (
 			<form className="todo-form" onSubmit={this.handleSubmit}>
-				<input type="text" placeholder="Что нужно сделать?" ref="title"/>
+				<input
+					type="text"
+					placeholder="Что нужно сделать?"
+					value={this.state.title}
+					onChange={this.handleChange}
+				/>
 
 				<Button type="submit"> Добавить </Button>
 			</form>
@@ -33,7 +46,7 @@ class Form extends Component {
 }
 
 Form.propTypes = {
-	onAdd: React.PropTypes.func.isRequired
+	onAdd: React.PropTypes.func.isRequired,
 };
 
 export default Form;
